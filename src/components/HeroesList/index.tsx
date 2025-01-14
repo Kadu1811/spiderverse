2c6963d745f2e3daf1a8b874af0c13d9f17713cb
Karlos Eduardo Plentis furtado <karlosfurtado@fitec.org.br>
2025-01-14 13:00:33 -0400

    chore: improve herolist

--- src/components/HeroesList/index.tsx
+++ src/components/HeroesList/index.tsx
@@ -1,6 +1,11 @@
+"use client";
+
 import { spidermanFont } from "@/fonts";
 import { IHeroData } from "@/interfaces/heroes";
 import styles from "./heroesList.module.scss";
+import { HeroPictures } from "../HeroPictures";
+
+import { motion } from "framer-motion";
 
 interface IProps {
   heroes: IHeroData[];
@@ -9,7 +14,33 @@ interface IProps {
 const HeroesList = ({ heroes }: IProps) => {
   return (
     <>
-      <h1 className={`${spidermanFont.className} ${styles.title}`}>Spiders</h1>
+      <motion.h1
+        className={`${spidermanFont.className} ${styles.title}`}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 2, delay: 2 }}
+      >
+        Spiders
+      </motion.h1>
+
+      <motion.section
+        className={styles.heroes}
+        initial={{ opacity: 0, y: -100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 2 }}
+      >
+        {heroes.map((hero, i) => (
+          <motion.div
+            key={i}
+            className={`${styles.imageContainer} ${styles[hero.id]}`}
+            whileHover={{ scale: 1.3 }}
+            whileTap={{ scale: 0.8 }}
+            transition={{ duration: 0.8 }}
+          >
+            <HeroPictures hero={hero} />
+          </motion.div>
+        ))}
+      </motion.section>
     </>
   );
 };
