99e2ded85a50bd5e8586d53b7460990f9efbd32b
Karlos Eduardo Plentis furtado <karlosfurtado@fitec.org.br>
2025-01-15 17:59:02 -0400

    feat: create carousel & effects

--- src/components/HeroesCarousel/index.tsx
+++ src/components/HeroesCarousel/index.tsx
@@ -1,5 +1,17 @@
+"use client";
+
 import { IHeroData } from "@/interfaces/heroes";
 import HeroDetails from "../HeroesDetails";
+import styles from "./carousel.module.scss";
+import { useEffect, useState } from "react";
+import { HeroPictures } from "../HeroPictures";
+import { AnimatePresence, motion } from "framer-motion";
+
+enum enPosition {
+  FRONT = 0,
+  MIDLE = 1,
+  BACK = 2,
+}
 
 interface IProps {
   heroes: IHeroData[];
@@ -7,11 +19,107 @@ interface IProps {
 }
 
 export default function HeroesCarousel({ heroes, activeId }: IProps) {
-  return (
-    <>
-      <h1>carousel {activeId}</h1>
+  const [visibleItems, setVisibleItems] = useState<IHeroData[] | null>(null);
+  const [activeIndex, setActiveIndex] = useState<number>(
+    heroes.findIndex((hero) => hero.id === activeId) - 1
+  );
 
-      <HeroDetails data={heroes[0]} />
-    </>
+  useEffect(() => {
+    const indexInArrayScope =
+      ((activeIndex % heroes.length) + heroes.length) % heroes.length;
+
+    const visibleItems = [...heroes, ...heroes].slice(
+      indexInArrayScope,
+      indexInArrayScope + 3
+    );
+
+    setVisibleItems(visibleItems);
+  }, [activeIndex, heroes]);
+
+  useEffect(() => {
+    const htmlEl = document.querySelector("html");
+
+    if (!htmlEl || !visibleItems) {
+      return;
+    }
+
+    const currentHeroId = visibleItems[enPosition.MIDLE].id;
+    htmlEl.style.backgroundImage = `url("/spiders/${currentHeroId}-background.png")`;
+    htmlEl.classList.add("hero-page");
+
+    return () => {
+      htmlEl.classList.remove("hero-page");
+    };
+  }, [visibleItems]);
+
+  const handleChangeActiveindex = (newDirection: number) => {
+    setActiveIndex((prevActiveIndex) => prevActiveIndex + newDirection);
+  };
+
+  if (!visibleItems) {
+    return null;
+  }
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.carousel}>
+        <div
+          className={styles.wrapper}
+          onClick={() => handleChangeActiveindex(1)}
+        >
+          <AnimatePresence mode="popLayout">
+            {visibleItems.map((item, position) => (
+              <motion.div
+                key={item.id}
+                className={styles.hero}
+                initial={{ x: -1500, scale: 0.75 }}
+                animate={{ x: 0, ...getItemsStyles(position) }}
+                exit={{ x: 0, opacity: 0, scale: 1, left: "-20%" }}
+                transition={{ duration: 0.8 }}
+              >
+                <HeroPictures hero={item} />
+              </motion.div>
+            ))}
+          </AnimatePresence>
+        </div>
+      </div>
+
+      <motion.div
+        className={styles.details}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 1, duration: 2 }}
+      >
+        <HeroDetails data={heroes[0]} />
+      </motion.div>
+    </div>
   );
 }
+
+const getItemsStyles = (position: enPosition) => {
+  if (position === enPosition.FRONT) {
+    return {
+      zIndex: 3,
+      filter: "blur(10px)",
+      scale: 1.2,
+    };
+  }
+
+  if (position === enPosition.MIDLE) {
+    return {
+      zIndex: 2,
+      left: 300,
+      scale: 0.8,
+      top: "-10%",
+    };
+  }
+
+  return {
+    zIndex: 1,
+    filter: "blur(10px)",
+    left: 160,
+    top: "-20%",
+    scale: 0.6,
+    opacity: 0.8,
+  };
+};
