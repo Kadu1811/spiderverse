9298aca9fc5145122eeab96dbd0f5069810cd534
Karlos Eduardo Plentis furtado <karlosfurtado@fitec.org.br>
2025-01-15 17:58:35 -0400

    chore: rename mulher aranha para spider gwen

--- src/components/HeroPictures/index.tsx
+++ src/components/HeroPictures/index.tsx
@@ -2,7 +2,7 @@ import { IHeroData } from "@/interfaces/heroes";
 
 import Image, { StaticImageData } from "next/image";
 import ImageSpiderMan616 from "@public/spiders/spider-man-616.png";
-import ImageSpiderWoman65 from "@public/spiders/mulher-aranha-65.png";
+import ImageSpiderWoman65 from "@public/spiders/spider-gwen-65.png";
 import ImageSpiderMan1610 from "@public/spiders/spider-man-1610.png";
 import ImageSpDr14512 from "@public/spiders/sp-dr-14512.png";
 import ImageSpiderHam8311 from "@public/spiders/spider-ham-8311.png";
