bd13c325cff3ab965b20f9df92ee73d543d5684c
Karlos Eduardo Plentis furtado <karlosfurtado@fitec.org.br>
2025-01-16 15:59:41 -0400

    chore: change domain origin

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,7 @@ const nextConfig: NextConfig = {
   env: {
     API_URL: "https://67868f4df80b78923aa7595d.mockapi.io",
     DOMAIN_ORIGIN:
-      process.env.NODE_ENV === "production"
-        ? "https://spiderverse-3p1pdf1zj-kadus-projects-62123fb7.vercel.app/"
-        : "http://localhost:3000",
+      "https://spiderverse-3p1pdf1zj-kadus-projects-62123fb7.vercel.app",
   },
 };
 
