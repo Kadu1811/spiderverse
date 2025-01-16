bcf709e23c7638d6493f02fdc590703bac5ae16e
Karlos Eduardo Plentis furtado <karlosfurtado@fitec.org.br>
2025-01-16 14:25:08 -0400

    fix: styles bug & id

--- src/app/hero/[id]/page.tsx
+++ src/app/hero/[id]/page.tsx
@@ -7,7 +7,8 @@ interface IProps {
   };
 }
 
-export default async function HeroPage({ params: { id } }: IProps) {
+export default async function HeroPage({ params }: IProps) {
+  const { id } = params;
   const heroes = await getHeroesData();
 
   return <HeroesCarousel heroes={heroes.data} activeId={id} />;
