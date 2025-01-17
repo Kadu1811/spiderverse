c1bb92e27d86986e0b1c5c69cdba3e49c8029cda
Karlos Eduardo Plentis furtado <karlosfurtado@fitec.org.br>
2025-01-17 09:57:05 -0400

    fix: endpoint

--- src/app/api/heroes/spiders.tsx
+++ src/app/api/heroes/spiders.tsx
@@ -1,11 +1,13 @@
 import { IHeroData } from "@/interfaces/heroes";
 
 export default async function getHeroesData(): Promise<{ data: IHeroData[] }> {
-  const res = await fetch(`${process.env.DOMAIN_ORIGIN}/api/heroes`);
+  const res = await fetch(`${process.env.API_URL}/api/heroes`);
+
+  const data = await res.json();
 
   if (!res.ok) {
     throw new Error("Failed to request heroes list");
   }
 
-  return res.json();
+  return { data };
 }
