97a036b8e775f8efe477f729eaac87619f0e8ad8
Karlos Eduardo Plentis furtado <karlosfurtado@fitec.org.br>
2025-01-16 23:08:12 -0400

    create env

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   env: {
     API_URL: "https://67868f4df80b78923aa7595d.mockapi.io",
-    DOMAIN_ORIGIN:
-      "https://spiderverse-3p1pdf1zj-kadus-projects-62123fb7.vercel.app",
+    DOMAIN_ORIGIN: "http://localhost:3000",
+    // "https://spiderverse-3p1pdf1zj-kadus-projects-62123fb7.vercel.app",
   },
 };
 
